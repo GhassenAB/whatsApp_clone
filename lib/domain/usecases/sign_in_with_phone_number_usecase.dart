f5c1fb5cd7dc43baab07fed7ce8d3f79d5797d81
Ghassen <ghassen.abbes@gmail.com>
2020-11-11 13:59:59 +0100

    setup clean architecture

---
+++ lib/domain/usecases/sign_in_with_phone_number_usecase.dart
@@ -0,0 +1,11 @@
+import 'package:whatsapp_clone/domain/repositories/firebase_repository.dart';
+
+class SignInWithPhoneNumberUseCase {
+  final FirebaseRepository repository;
+
+  SignInWithPhoneNumberUseCase({this.repository});
+
+  Future<void> call(String smsPinCode) async {
+    return await repository.signInWithPhoneNumber(smsPinCode);
+  }
+}
---
+++ lib/domain/usecases/sign_out_usecase.dart
@@ -0,0 +1,11 @@
+import 'package:whatsapp_clone/domain/repositories/firebase_repository.dart';
+
+class SignOutUseCase {
+  final FirebaseRepository repository;
+
+  SignOutUseCase({this.repository});
+
+  Future<void> call() async {
+    return await repository.signOut();
+  }
+}
